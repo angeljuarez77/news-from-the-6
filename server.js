7f5d0a13596d282b410d3b40e6414872769cb9ff
angel juarez <angeljuarez729@gmail.com>
2019-01-04 17:58:56 -0500

    could in theory log in and get a token back from insomnia. So it should work for front end

--- server.js
+++ server.js
@@ -14,13 +14,26 @@ app.use(bodyParser.json());
 app.use(cors());
 
 app.get('/', (req, res) => res.send('Hello World!'));
-app.get('/loggedin', async (req, res) => {
-    try { 
-        const posts = await Post.findAll();
-        res.json({ posts });
-    } catch(e){console.log(e)}
-});
-
+app.post('/', async (req, res) => {
+    try {
+        const attempt = await User.findAll({
+            where: {
+            email: req.body.email
+        }});
+       const userresp = attempt[0].dataValues;
+       const validation = bcrypt.compareSync(req.body.password, userresp.password);
+       const { user_name, password, access_level } = userresp;
+       if(validation){
+           const jwt = sign({
+               user_name,
+               access_level
+           });
+           res.json({ jwt });
+       } else { 
+           res.json({message: "You got it wrong!"})
+       }
+    } catch (e) { console.log(e) }
+})
 app.get('/users', async (req, res) => {
     try {
         const users = await User.findAll();
@@ -32,14 +45,14 @@ app.post('/users', async (req, res) => {
     try {
         const user = req.body;
         await User.create(user);
-        const { user_name, email } = user;
+        const { user_name, email, access_level } = user;
         const token = sign({
             user_name, 
-            email
+            email,
+            access_level
         })
         res.json({ user, token });
     } catch (e) {console.log(e)}
 });
-
 
 app.listen(PORT, () => console.log(`Example app listening on port ${PORT}!`));
